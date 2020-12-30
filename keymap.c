485e700bb7f7ae1f4be6881da6beb8919822360a
claudiohbsantos <claudiohbsantos@gmail.com>
2020-12-30 10:42:37 -0500

    single key alt-tab

--- keymap.c
+++ keymap.c
@@ -278,7 +278,7 @@ void process_combo_event(uint16_t combo_index, bool pressed) {
 // #define SFT_A LSFT_T(KC_A )
 // #define SFT_SC RSFT_T(KC_SCLN )
 #define SFT_QT RSFT_T(KC_QUOT)
-
+#define A_TAB LALT(KC_TAB)
 
 #define OSM_SFT OSM(MOD_LSFT)
 
@@ -292,7 +292,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_TAB  ,  KC_Q   ,  KC_W   ,  KC_E   ,  KC_R   ,  KC_T   ,            KC_Y   ,  KC_U   ,  KC_I   ,  KC_O   ,  KC_P   ,  KC_MINS  ,
       OSM_SFT ,  KC_A   ,  KC_S   ,  KC_D   ,  KC_F   ,  KC_G   ,            KC_H   ,  KC_J   ,  KC_K   ,  KC_L   ,  KC_SCLN ,  SFT_QT   ,
       KC_DEL  ,  KC_Z   ,  KC_X   ,  KC_C   ,  KC_V   ,  KC_B   ,            KC_N   ,  KC_M   ,  KC_COMM, KC_DOT  ,  KC_SLSH,  KC_BSLASH,
-                           KC_GRV , _______ ,                                                    KC_PLUS, KC_EQL  ,
+                           KC_GRV ,  A_TAB  ,                                                    KC_PLUS, KC_EQL  ,
                                    LT(_RAISE, KC_TAB) , KC_SPC  ,            KC_ENT , OSL(_LOWER),
                                               KC_LALT , KC_LCTL ,            KC_LCTL, KC_LALT  ,
                                           TG(_PLOVER) , KC_LGUI ,            KC_LGUI, KC_CAPS
