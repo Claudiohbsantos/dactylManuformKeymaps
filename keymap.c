11010d3ed3b8ca1ce51a54234e187588a10bac52
Claudio Santos <Claudiohbsantos@users.noreply.github.com>
2021-09-08 10:12:41 -0400

    remove unused brackets

--- keymap.c
+++ keymap.c
@@ -285,8 +285,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_RAISE] = LAYOUT_5x6(
      KC_F12  ,  KC_F1  ,  KC_F2  ,  KC_F3  ,  KC_F4  ,  KC_F5  ,             KC_F6   ,  KC_F7  ,  KC_F8  ,  KC_F9  , KC_F10  , KC_F11  ,
-     _______ , KC_MPRV , KC_MUTE , KC_MPLY , KC_MNXT , KC_LBRC ,             KC_RBRC , _______ , KC_UP   , _______ , _______ , KC_MUTE ,
-     _______ , KC_LEFT , KC_UP   , KC_DOWN , KC_RGHT , KC_LPRN ,             KC_RPRN , KC_LEFT , KC_DOWN , KC_RGHT , _______ , _______ ,
+     _______ , KC_MPRV , KC_MUTE , KC_MPLY , KC_MNXT , _______ ,             _______ , _______ , KC_UP   , _______ , _______ , KC_MUTE ,
+     _______ , KC_LEFT , KC_UP   , KC_DOWN , KC_RGHT , _______ ,             _______ , KC_LEFT , KC_DOWN , KC_RGHT , _______ , _______ ,
      KC_PSCR  , _______ , _______ , _______ , _______ , _______ ,             KC_TAB  , _______ , _______ , _______ , _______ , _______ ,
                          KC_VOLD , KC_VOLU ,                                                     _______ , _______ ,
                                              _______ , _______ ,             _______ , MO(_DEV) ,
